cb62b39b3d1f9a4312d41cc9018055e30ba24c0c
David O Shea <osheadavid7@gmail.com>
2014-05-12 15:31:09 -0600

    can retrieve info from linkedin

---
+++ first_go.py
@@ -0,0 +1,31 @@
+#Append path and import
+import sys
+sys.path.append('../python3-linkedin/')
+from linkedin import linkedin
+
+#Import secrets
+from secrets import secret
+
+
+CONSUMER_KEY = secret['API_KEY']
+CONSUMER_SECRET = secret['Secret_Key']
+USER_TOKEN = secret['OAUT']
+USER_SECRET = secret['OAUS']
+RETURN_URL = 'http://localhost:8000'
+
+
+authentication = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET,
+                                                          USER_TOKEN, USER_SECRET,
+                                                          RETURN_URL, linkedin.PERMISSIONS.enums.values())
+
+# Pass it in to the app...
+
+application = linkedin.LinkedInApplication(authentication)
+
+# Use the app....
+
+g = application.get_profile()
+#print( g )
+#
+u1 = g['siteStandardProfileRequest']['url']
+d1 = urllib.request.urlopen(u1)
