a3745a5968730b58ef72833c0c709d74aa382b49
David O Shea <osheadavid7@gmail.com>
2014-05-15 18:44:04 +0100

    functionial-izing

--- python2_att.py
+++ python2_att.py
@@ -13,13 +13,29 @@ USER_TOKEN = secret['OAUT']
 USER_SECRET = secret['OAUS']
 RETURN_URL = 'http://localhost:8000'
 
-
-authentication = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET,
+def fetch_newest_data():
+    authentication = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET,
                                                           USER_TOKEN, USER_SECRET,
                                                           RETURN_URL, linkedin.PERMISSIONS.enums.values())
-# Pass it in to the app...
-application = linkedin.LinkedInApplication(authentication)
+    # Pass it in to the app...
+    application = linkedin.LinkedInApplication(authentication)
+    
+    # Use the app....
+    app_data=application.get_profile(selectors=['id', 'first-name', 'last-name', 'location', 'distance', 'num-connections', 'skills', 'educations','interests'])
+    return app_data
 
-# Use the app....
-g = application.get_profile()
-add_data=application.get_profile(selectors=['id', 'first-name', 'last-name', 'location', 'distance', 'num-connections', 'skills', 'educations'])
+
+#Fetch current profile
+app_data = fetch_newest_data() 
+
+#Separate items
+education = app_data['educations']
+names = [app_data['firstName'],app_data['lastName']]
+
+#Strip skills
+skill_list =  app_data['skills']
+skills = [skill['skill']['name'] for skill in skill_list['values']]
+
+
+interests_lists = app_data['interests']
+interests = interests_lists['interests'].split(',')
