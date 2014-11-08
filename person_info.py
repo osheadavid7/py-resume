629bdfce4729092c0ec6ff7c43f40fad1314f58d
David O Shea <osheadavid7@gmail.com>
2014-11-08 20:31:29 +0000

    added dummy secrets and person_info files as per request

---
+++ person_info.py
@@ -0,0 +1,1 @@
+person_info={'mob_num':'123456789','email':'someemail@gmail.com'}
---
+++ secrets.py
@@ -0,0 +1,3 @@
+secret = {'API_KEY':'linkedin_api_key_here...','Secret_Key':'linkedin_secret_key_here',
+'OAUT':'linkedin_suer_token_OUAT_here',
+          'OAUS':'linkedin_user_secret_OAUS_here','GU':'github_user','GP':'github_pass'}
